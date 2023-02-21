f1b605a47597cafbe68d97acff0503b9c3add4aa
Esteban Orrego <orregoe@gmail.com>
2023-02-20 23:09:30 -0300

    agrego ruta post con saludo con json y urlencoded

--- express.js
+++ express.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.end("Hola mundo! con express");
+});
+
+app.post('/saludos', (req, res) => {
+    res.send(`Hola ${req.body.name}!`);
 });
 
 app.listen(3040);
