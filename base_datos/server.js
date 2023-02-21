3271de00ed64b4d70e185d51b87494b70fbfa8df
Esteban Orrego <orregoe@gmail.com>
2023-02-21 01:42:33 -0300

    agregamos ORM sequelize, conecta nodejs y sqlite

--- base_datos/server.js
+++ base_datos/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 const app = express();
 
@@ -8,19 +9,24 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let db = new sqlite3.Database('proyecto-backend');
+const sequelize = new Sequelize('proyecto-backend',null,null,{
+    dialect: 'sqlite',
+    storage: './proyecto-backend'
+});
+
+//let db = new sqlite3.Database('proyecto-backend');
 
 //db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))');
 
 app.post('/pendientes', (req, res) => {
-    db.run(`INSERT INTO tasks(description) VALUES(?)`, req.body.description);
+    //db.run(`INSERT INTO tasks(description) VALUES(?)`, req.body.description);
     res.send("Tarea agregada");
 });
 
 app.listen(3050);
 
-process.on('SIGINT', () => {
-    console.log("Cerrando la base de datos");
-    db.close();
-    process.exit();
-});
+//process.on('SIGINT', () => {
+//    console.log("Cerrando la base de datos");
+//    db.close();
+//    process.exit();
+//});
